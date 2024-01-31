f917a0c8856d75f16a3cbf5d5172389190e919f4
Afin Tachtiar <afintachtiar@apps.ipb.ac.id>
2024-01-31 11:36:27 +0700

    day03

--- day01/task01.js
+++ day01/task01.js
@@ -1,1 +1,17 @@
-// masih sepi :()
+const formWrapper = document.getElementById("form-wrapper");
+
+const editForm = document.getElementById("edit-form");
+editForm.addEventListener("click", () => {
+  formWrapper.style.display = "block";
+});
+
+const form = document.getElementById("identity-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let fieldValues = document.getElementsByClassName("form-control");
+  let identityValues = document.getElementsByClassName("identity-value");
+  Array.from(fieldValues).forEach((element, index) => {
+    identityValues[index].innerHTML = element.value;
+  });
+  formWrapper.style.display = "none";
+});
